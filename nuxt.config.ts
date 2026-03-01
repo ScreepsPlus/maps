d6f4f6fd5bd7d727bdad044f286f6e4bb75b27fd
AlinaNova21 <alina@alinanova.dev>
2026-03-01 04:05:00 -0600

    Configure hub drivers and worker name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,17 +3,21 @@ export default defineNuxtConfig({
   runtimeConfig: {
     uploadToken: '',
   },
-  hub: {
-    blob: true,
-    kv: true,
-  },
   nitro: {
     cloudflare: {
       wrangler: {
         name: 'screepsplus-maps',
-        kv_namespaces: [{ binding: 'KV', id: '33681fe71b45423d9454a2b07ca59660' }],
-        r2_buckets: [{ binding: 'BLOB', bucket_name: 'screepsplus-maps' }],
       },
+    },
+  },
+  hub: {
+    kv: {
+      driver: 'cloudflare-kv-binding',
+      namespaceId: '33681fe71b45423d9454a2b07ca59660',
+    },
+    blob: {
+      driver: 'cloudflare-r2',
+      bucketName: 'screepsplus-maps',
     },
   },
 })
